a57a3c1c575a83f3a8f6ab3225f77ca8316fbffa
Marian-Ovidiu Hutanu <h.marian914@gmail.com>
2025-09-09 11:29:09 +0200

    Aggiustato diario di bordo

--- wp-config.php
+++ wp-config.php
@@ -23,9 +23,9 @@
 define( 'DB_USER', 'u597020236_783vB' );
 define( 'DB_PASSWORD', 'projectAfricaC2024');
 
-//define('DB_NAME', 'pac');
-//define('DB_USER', 'root');
-//define('DB_PASSWORD', 'root');
+// define('DB_NAME', 'pac');
+// define('DB_USER', 'root');
+// define('DB_PASSWORD', 'root');
 
 // define('WP_HOME', 'https://project-africa-conservation.org');
 // define('WP_SITEURL', 'https://project-africa-conservation.org');
