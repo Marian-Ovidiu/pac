95a8ccfef4f092541a5362b80ccc4072285261dd
Marian Ovidiu Hutanu <h.marian914@gmail.com>
2026-07-30 10:09:21 +0200

    chore: consolidate WordPress plugin updates

--- wp-content/plugins/polylang/vendor/composer/installed.php
+++ wp-content/plugins/polylang/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wpsyntex/polylang',
         'pretty_version' => '3.8.x-dev',
         'version' => '3.8.9999999.9999999-dev',
-        'reference' => 'e6964fb795ad5b430219977a75f2e4ca9d55fb52',
+        'reference' => 'fc57034e56072d860bb3ed59d36e705cdee15a3c',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'wpsyntex/polylang' => array(
             'pretty_version' => '3.8.x-dev',
             'version' => '3.8.9999999.9999999-dev',
-            'reference' => 'e6964fb795ad5b430219977a75f2e4ca9d55fb52',
+            'reference' => 'fc57034e56072d860bb3ed59d36e705cdee15a3c',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
